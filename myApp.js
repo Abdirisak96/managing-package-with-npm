77080ea987f660ee4e395b0a8710a2053065639d
Abdirisak Mohamed <abdirisak.mohamed1@hotmail.com>
2022-01-10 12:51:07 +0000

    add Hello World to myApp

---
+++ myApp.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var app = express();
+var bGround = require("fcc-express-bground");
+
+console.log("Hello World");
+bGround.log("Hello World");
+module.exports = app;
