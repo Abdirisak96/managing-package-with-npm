7b8447ea8b84975f8eff815f6e90fcd9ab1daceb
Abdirisak Mohamed <abdirisak.mohamed1@hotmail.com>
2022-01-11 11:32:06 +0000

    added .env file

--- myApp.js
+++ myApp.js
@@ -1,7 +1,28 @@
+require("dotenv").config();
 var express = require("express");
 var app = express();
 var bGround = require("fcc-express-bground");
 
 console.log("Hello World");
 bGround.log("Hello World");
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", function (req, res) {
+  res.json({ message: "Hello json" });
+});
+
+app.get("/json", function (req, res) {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "hello json" });
+  }
+});
+
 module.exports = app;
